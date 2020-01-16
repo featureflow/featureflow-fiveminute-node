5e080db57883ecdd542f422aff288ce2fcdba56c
Oliver Oldfield-Hodge <ooldfieldhodge@atlassian.com>
2020-01-16 12:34:06 +1100

    evaluateAll

--- helloworld.js
+++ helloworld.js
@@ -4,11 +4,12 @@ var Featureflow = require('featureflow-node-sdk');
 var featureflow = new Featureflow.Client({apiKey: '{{YOUR_SERVER_ENVIRONMENT_API_KEY_HERE}}'});
 
 featureflow.ready(function(){
+    console.log(featureflow.evaluateAll())
     if (featureflow.evaluate('example-feature').isOn()) {
-        console.log('The variant is on!');
+        console.log('The example-feature variant is on!');
     }
     else {
-        console.log('The variant is not on!');
+        console.log('The example-feature variant is not on!');
     }
     //featureflow is now initialized in this block
 });
